7079da31f6348b31bef51baa0019668cb6f26a82
Chloe <chloeleichen@gmail.com>
2016-06-19 00:46:13 +1000

    clean up

--- mario.c
+++ mario.c
@@ -8,15 +8,34 @@ super mario ladders
 #####
 */
 
+void makeLadders(int);
 int main()
 {
-    int height = 8;
+    int n;
+
+
+    do
+    {
+        printf("Please input an integer value between 1 and 30: ");
+        scanf("%d", &n);
+    }
+    while (n < 1 || n > 30);
+
+    makeLadders(n);
+    return 0;
+}
+
+void makeLadders(int n)
+{
+    int height = n;
     int column = height + 1;
+    int test = 0;
 
     for(int i = 0; i < height; i ++)
     {
+        test ++;
         for (int j = 0; j < column; j ++)
-        {
+        {   
             if(j < column - i - 2){
                 printf("%s", " ");
             }
@@ -29,8 +48,9 @@ int main()
             
         }
     }
-    
+    printf("the height of the ladder is %d\n", test);
 }
+
 
 
 
