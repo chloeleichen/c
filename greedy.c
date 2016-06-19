da957bab0d83a3feefcff6505a27072657fc1036
Chloe <chloeleichen@gmail.com>
2016-06-19 13:33:03 +1000

    simplify

---
+++ greedy.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+/*
+  given the change required 
+  print the list amount of coin needed
+ */
+
+int coinsAvailable[4] = {25, 10, 5, 1};
+
+int getCoins(int);
+int main(){
+    float input;
+    int cents;
+    int dollars;
+    int n;
+
+    printf("How much do I owe you ?\n");
+    scanf("%f", &input);
+
+    input = round(input*100);
+    cents = input;
+    cents = cents%100;
+
+    n = getCoins(cents);
+    printf("number of coins : %d\n",n);
+    return 0;
+}
+
+int getCoins(int cents){
+    int n = 0;
+    printf("cents: %d\n", cents);
+    for(int i = 0; i < 4; i ++){
+        while (cents >= coinsAvailable[i])
+        {
+            cents -= coinsAvailable[i];
+            printf("give you: %d\n",coinsAvailable[i]);
+            n ++;
+        };
+    }
+    return n;
+}
+
+
